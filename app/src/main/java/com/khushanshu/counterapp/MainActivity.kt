d5be84a480d3f5641f67dc51f1533cf9a2599bb0
Khushanshu Yadav <khushanshuyadav2@gmail.com>
2024-05-01 00:17:50 +0530

    First Commit : App without MVVM cannot save the state

--- app/src/main/java/com/khushanshu/counterapp/MainActivity.kt
+++ app/src/main/java/com/khushanshu/counterapp/MainActivity.kt
@@ -3,13 +3,27 @@ package com.khushanshu.counterapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.khushanshu.counterapp.ui.theme.CounterAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    CounterApp()
 
                 }
             }
@@ -29,3 +44,37 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun CounterApp(){
+
+    val count= remember{mutableStateOf(0)}
+    fun increment(){
+        count.value++;
+    }
+    fun decrement(){
+        count.value--;
+    }
+
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Text(text = "Count : ${count.value}", fontSize = 48.sp,fontWeight = FontWeight.Bold);
+
+        Spacer(modifier = Modifier.height(16.dp));
+
+        Row {
+            Button(onClick = { increment() }) {
+                Text(text = "INCREMENT")
+            }
+
+            Spacer(modifier = Modifier.width(16.dp));
+
+            Button(onClick = {decrement()}) {
+                Text(text = "DECREMENT")
+            }
+        }
+
+
+
+    }
+
+}
